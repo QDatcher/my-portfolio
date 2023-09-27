dd2124bbfefe5db6ed1ca3c9eadb1ba16583fab7
Quise <darkquise@gmail.com>
2023-09-26 21:39:37 -0400

    okay

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://10.10.10.136:27017/my-portfolio');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/my-portfolio');
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 
 
   const startApolloServer = async () => {
+    console.log(process.env)
     await server.start();
     server.applyMiddleware({ app });
     
