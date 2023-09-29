a9d1976a6ade5319e591b1468c03263999c81b33
Quise <darkquise@gmail.com>
2023-09-29 01:04:20 -0400

    help

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://quentindatcher:B4ckw00d1324%24@cluster0.brq1qp5.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://quentindatcher:B4ckw00d1324%24@cluster0.brq1qp5.mongodb.net/my-portfolio');
 
 module.exports = mongoose.connection;
